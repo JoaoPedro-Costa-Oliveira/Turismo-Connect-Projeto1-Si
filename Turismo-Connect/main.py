11101b1e78138741f3708753147e1ff2ee00dbac
JoaoPedro-Costa-Oliveira <168649576+JoaoPedro-Costa-Oliveira@users.noreply.github.com>
2024-09-13 12:45:08 -0300

    main.py

--- Turismo-Connect/main.py
+++ Turismo-Connect/main.py
@@ -1,6 +1,5 @@
-import menu # importando o codigo menu que vai ser executado
+import menu 
 import os
 os.system("cls")
-menu = menu.Menu() # criando uma variavel menu e chamnando a classe Menu
-
-menu.menu() # atribuindo função para a variavel 
+menu = menu.Menu() 
+menu.menu()  
